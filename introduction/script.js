ed8b09f4a9dc9fdf99b37e0a8584d141f95337c3
tcvduc <caovanducs@gmail.com>
2022-09-13 14:36:51 +0700

    update

--- introduction/script.js
+++ introduction/script.js
@@ -1,7 +1,47 @@
 (function () {
-  const classes = {};
+  const classes = {
+    contact: "contact",
+    contactContent: "contact-content",
+    active: "active",
+    iconCopy: "icon-copy",
+    copySuccessClipboard: "copy-success-clipboard",
+  };
 
-  function main() {}
+  /**
+   *
+   * @param {HTMLElement[]} contacts
+   * @param {HTMLElement[]} iconCopies
+   */
+  function handleContactOnclick(contacts, iconCopies) {
+    for (let i = contacts.length - 1; i >= 0; --i) {
+      contacts[i].onclick = function () {
+        for (let j = contacts.length - 1; j >= 0; --j) {
+          if (j !== i) {
+            iconCopies[j].classList.remove(classes.copySuccessClipboard);
+
+            contacts[j].classList.remove(classes.active);
+          }
+        }
+
+        contacts[i].classList.add(classes.active);
+
+        const contactInformation = contacts[i].children[0].textContent;
+
+        // copy text to clipboard
+        navigator.clipboard.writeText(contactInformation);
+
+        // change color copy icon
+        iconCopies[i].classList.add(classes.copySuccessClipboard);
+      };
+    }
+  }
+
+  function main() {
+    const contacts = document.getElementsByClassName(classes.contact);
+    const iconCopies = document.getElementsByClassName(classes.iconCopy);
+
+    handleContactOnclick(contacts, iconCopies);
+  }
 
   main();
 })();
