989b81d7546e5a3e3b43a95f7e7847709c354f0b
tcvduc <caovanducs@gmail.com>
2022-09-14 15:57:15 +0700

    train

--- portfolio/7.split-landing-page/solve-1/7.js
+++ portfolio/7.split-landing-page/solve-1/7.js
@@ -1,16 +1,135 @@
 (function () {
   const classes = {
-    width25Percent: "width-25-percent",
-    width75Percent: "width-75-percent",
-    column: "column",
+    width80Percent: "width-80-percent",
+    width40Percent: "width-40-percent",
+    column1: "column-1",
+    column2: "column-2",
+    zIndex10: "z-index-10",
+    zIndex15: "z-index-15",
     scale1dot2: "scale-1dot2",
     layerBackground1: "layer-background-1",
     layerBackground2: "layer-background-2",
   };
 
+  class Coordinate {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
   /**
    *
-   * @param {HTMLElement[]} columns
+   * @param {HTMLElement} element
+   */
+  function getElementArea(element) {
+    const width = element.getBoundingClientRect().width;
+    const height = element.getBoundingClientRect().height;
+    const offsetLeft = element.getBoundingClientRect().x;
+    const offsetTop = element.getBoundingClientRect().y;
+
+    const minimumElementAreaCoordinate = [offsetLeft, offsetTop];
+    const maximumElementAreaCoordinate = [
+      offsetLeft + width,
+      offsetTop + height,
+    ];
+
+    return {
+      minimumElementAreaCoordinate,
+      maximumElementAreaCoordinate,
+    };
+  }
+
+  /**
+   *
+   * @param {HTMLElement} column1
+   * @param {Coordinate} mouseCoordinate
+   */
+  function detectWasMouseInColumn1Area(column1, mouseCoordinate) {
+    const column1Area = getElementArea(column1);
+
+    const [x1, y1] = [
+      column1Area.minimumElementAreaCoordinate[0],
+      column1Area.minimumElementAreaCoordinate[1],
+    ];
+
+    const [x2, y2] = [
+      column1Area.maximumElementAreaCoordinate[0],
+      column1Area.maximumElementAreaCoordinate[1],
+    ];
+
+    let flag1 = false;
+    let flag2 = false;
+
+    if (mouseCoordinate.x >= x1 && mouseCoordinate.x <= x2) {
+      flag1 = true;
+    }
+
+    if (mouseCoordinate.y >= y1 && mouseCoordinate.y <= y2) {
+      flag2 = true;
+    }
+
+    if (flag1 && flag2) {
+      console.log(1);
+      document.getElementsByClassName("layer-1")[0].style.border =
+        "10px solid #fafafa";
+      return;
+    }
+
+    if (!(flag1 && flag2)) {
+      document.getElementsByClassName("layer-1")[0].style.border = "";
+      return;
+    }
+  }
+
+  /**
+   *
+   * @param {HTMLElement} column2
+   * @param {Coordinate} mouseCoordinate
+   *
+   *
+   */
+  function detectWasMouseInColumn2Area(column2, mouseCoordinate) {
+    const column2Area = getElementArea(column2);
+
+    const [x1, y1] = [
+      column2Area.minimumElementAreaCoordinate[0],
+      column2Area.minimumElementAreaCoordinate[1],
+    ];
+
+    const [x2, y2] = [
+      column2Area.maximumElementAreaCoordinate[0],
+      column2Area.maximumElementAreaCoordinate[1],
+    ];
+
+    const [x, y] = [mouseCoordinate.x, mouseCoordinate.y];
+
+    let flag1 = false;
+    let flag2 = false;
+
+    if (x >= x1 && x <= x2) {
+      flag1 = true;
+    }
+
+    if (y >= y1 && y <= y2) {
+      flag2 = true;
+    }
+
+    if (flag1 && flag2) {
+      document.getElementsByClassName("layer-2")[0].style.border =
+        "10px solid #fff";
+      return;
+    }
+
+    if (!(flag1 && flag2)) {
+      document.getElementsByClassName("layer-2")[0].style.border = "";
+    }
+  }
+
+  /**
+   *
+   * @param {HTMLElement} column1
+   * @param {HTMLElement} column2
    * @param {HTMLElement} layerBackground1
    * @param {HTMLElement} layerBackground2
    *
@@ -18,51 +137,49 @@
    *
    */
   function handleColumnOnMouseOver(
-    columns,
+    column1,
+    column2,
     layerBackground1,
     layerBackground2
   ) {
-    for (let i = columns.length - 1; i >= 0; --i) {
-      columns[i].onmouseover = function () {
-        for (let j = columns.length - 1; j >= 0; --j) {
-          if (j !== i) {
-            columns[j].classList.add(classes.width25Percent);
-          }
-        }
+    let x = 0;
+    let y = 0;
+    let mouseCoordinateDebug = "";
+    let mouseCoordinate = {
+      x: 0,
+      y: 0,
+    };
 
-        if (i === 0) {
-          layerBackground1.classList.add(classes.scale1dot2);
-        }
+    // detectColumn1Area(column1);
 
-        if (i === 1) {
-          layerBackground2.classList.add(classes.scale1dot2);
-        }
+    window.onmousemove =
+      /**
+       *
+       * @param {MouseEvent} event
+       */
+      function (event) {
+        const { pageX, pageY } = event;
 
-        columns[i].classList.add(classes.width75Percent);
+        x = pageX;
+        y = pageY;
+
+        mouseCoordinateDebug = `(${x},${y})`;
+
+        mouseCoordinate.x = x;
+        mouseCoordinate.y = y;
+
+        // console.log(mouseCoordinate);
+
+        detectWasMouseInColumn1Area(column1, mouseCoordinate);
+
+        detectWasMouseInColumn2Area(column2, mouseCoordinate);
       };
-
-      columns[i].onmouseleave = function () {
-        for (let k = columns.length - 1; k >= 0; --k) {
-          if (k !== i) {
-            columns[k].classList.remove(classes.width25Percent);
-          }
-        }
-
-        if (i === 0) {
-          layerBackground1.classList.remove(classes.scale1dot2);
-        }
-
-        if (i === 1) {
-          layerBackground2.classList.remove(classes.scale1dot2);
-        }
-
-        columns[i].classList.remove(classes.width75Percent);
-      };
-    }
   }
 
   function main() {
-    const columns = document.getElementsByClassName(classes.column);
+    const column1 = document.getElementsByClassName(classes.column1)[0];
+    const column2 = document.getElementsByClassName(classes.column2)[0];
+
     const layerBackground1 = document.getElementsByClassName(
       classes.layerBackground1
     )[0];
@@ -70,7 +187,12 @@
       classes.layerBackground2
     )[0];
 
-    handleColumnOnMouseOver(columns, layerBackground1, layerBackground2);
+    handleColumnOnMouseOver(
+      column1,
+      column2,
+      layerBackground1,
+      layerBackground2
+    );
   }
 
   main();
