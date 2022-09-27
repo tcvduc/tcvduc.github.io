16314b37f581b5c0404874fe0074a672c62bbbb7
tcvduc <caovanducs@gmail.com>
2022-09-27 12:46:04 +0700

    write road map for feature search portfolio, implemented some steps

--- feature-search-portfolio.js
+++ feature-search-portfolio.js
@@ -1,11 +1,31 @@
 (function () {
   /**
    * Feature: Search Portfolio
-   * + Allow to search Introduction Tab Content
-   * + Allow to search Portfolio Tab Content
+   * 1. Allow to search Introduction Tab Content
+   * 2. Allow to search Portfolio Tab Content
+   *
+   *
+   * -- ---------------------------------------------------------
+   * -- 1. Allow to search Introduction Tab Content
+   * -- ----------------------------------------------------------
+   * + step 1: get input keyword - done
+   * + step 2: advance regex trim step 1 - done
+   * + step 3: from parent html send a message string to children iframe - done
+   * + step 4: from parent html send step 2 ret to children iframe - done
+   * + step 5: define an array of strings which allow to search, ["brief", "introduction", "experience", "education", "contact"]
+   * + step 6: detect search keyword whether it is equal to step 5 or not, ignore computer case sensitive
+   * + step 7: if step 6 was true, scroll the web site to the keyword element position
+   * + step 8: hight light element contains keyword
+   *
+   *
+   *
    *
    *
    */
+
+  const tags = {
+    iframe: "iframe",
+  };
 
   const classes = {
     inputSearchPortfolio: "input-search-portfolio",
@@ -31,10 +51,49 @@
 
   /**
    *
-   * @param {HTMLIFrameElement} iframe
+   * @param {string} s
    */
-  function f(iframe) {
-    console.log(iframe.contentWindow.document);
+  function advanceRegexTrim(s) {
+    /**
+     * - s = "
+     *       abc     def
+     * "
+     * - ret = "abc def"
+     *
+     * + step 1: regex detect all `\n`, replace it by "" - done
+     *   + ret: "      abc     def    "
+     * + step 2: regex detect multiple spacing, replace it by " " - done
+     *   + ret: " abc def "
+     * + step 3: regex detect spacing in the begin of string - done
+     *   + ret: "abc def "
+     * + step 4: regex detect spacing at the end of string
+     *   + ret: "abc def"
+     *
+     *
+     */
+    const emptyString = "";
+    const oneSpacing = " ";
+    const regexAllDownLineSign = /\n+/g;
+    const regexDetectMultipleSpacing = / +/g;
+    const regexDetectSpacingInTheBeginning = /^ +/g;
+    const regexDetectSpacingAtTheEnd = / +$/g;
+
+    return s
+      .replace(regexAllDownLineSign, emptyString)
+      .replace(regexDetectMultipleSpacing, oneSpacing)
+      .replace(regexDetectSpacingInTheBeginning, emptyString)
+      .replace(regexDetectSpacingAtTheEnd, emptyString);
+  }
+
+  /**
+   *
+   * @param {HTMLIFrameElement} iframe
+   * @param {string} searchKeyword
+   *
+   *
+   */
+  function fromParentHtmlSendSearchKeywordToIframe(iframe, searchKeyword) {
+    iframe.contentWindow.postMessage(searchKeyword, "*");
   }
 
   /**
@@ -56,23 +115,28 @@
 
         if (key === "Enter") {
           const currentSidebarTabActive = detectSideBarActiveTab();
-          const iframe = document.getElementsByTagName("iframe")[0];
 
+          const iframe = document.getElementsByTagName("iframe")[0];
           const titles2 = document.getElementsByClassName("title");
 
-          console.log(inputValue);
-
-          f(iframe);
+          const advanceTrimInputValueRet = advanceRegexTrim(inputValue);
+          const searchKeyword = advanceTrimInputValueRet;
 
           switch (currentSidebarTabActive) {
             case sideBarTabName.Introduction:
+              // Tab Active: Introduction
               break;
 
             case sideBarTabName.Portfolio:
+              // Tab Active: Portfolio
               break;
 
             default:
+              // Default Tab Active: Introduction
               console.log("Tab Active: Introduction");
+
+              fromParentHtmlSendSearchKeywordToIframe(iframe, searchKeyword);
+
               break;
           }
         }
