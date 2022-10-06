91905fa8dac60015883f5d7fc464e256d00c21e8
tcvduc <caovanducs@gmail.com>
2022-10-06 13:44:34 +0700

    train form wave

--- portfolio-shred/8.form-wave/train/solve-1/8.js
+++ portfolio-shred/8.form-wave/train/solve-1/8.js
@@ -4,7 +4,9 @@
     titleForm: "titleForm",
     layerForm: "layerForm",
     inputEmail: "inputEmail",
+    inputPassword: "inputPassword",
     labelEmail: "labelEmail",
+    labelPassword: "labelPassword",
     labelOnFocus: "labelOnFocus",
     inputOnFocus: "inputOnFocus",
     width100Percent: "width100Percent",
@@ -81,33 +83,50 @@
 
   /**
    *
-   * @param {HTMLElement} inputEmail
-   * @param {HTMLElement} labelEmail
+   * @param {HTMLElement} input
+   * @param {HTMLElement} label
    *
    */
-  function addCSSForInputEmailWhenItFocused(inputEmail, labelEmail) {
-    inputEmail.classList.add(classes.inputOnFocus);
-    labelEmail.classList.add(classes.labelOnFocus);
+  function addCSSForInputWhenItFocused(input, label) {
+    input.classList.add(classes.inputOnFocus);
+    label.classList.add(classes.labelOnFocus);
   }
 
   /**
    *
-   * @param {HTMLElement} inputEmail
-   * @param {HTMLElement} labelEmail
+   * @param {HTMLElement} input
+   * @param {HTMLElement} label
    *
    */
-  function removeCSSForInputEmailWhenItFocused(inputEmail, labelEmail) {
-    inputEmail.classList.remove(classes.inputOnFocus);
-    labelEmail.classList.remove(classes.labelOnFocus);
+  function removeCSSForInputWhenItFocused(input, label) {
+    input.classList.remove(classes.inputOnFocus);
+    label.classList.remove(classes.labelOnFocus);
+  }
+
+  /**
+   *
+   * @param {string} s
+   */
+  function wasInputPasswordString(s) {
+    const regex = /inputPassword/g;
+    return regex.test(s);
   }
 
   /**
    *
    * @param {HTMLInputElement} inputEmail
    * @param {HTMLInputElement} labelEmail
+   * @param {HTMLInputElement} inputPassword
+   * @param {HTMLInputElement} labelPassword
+   *
    *
    */
-  function inputEmailTabKeyFocusedOn(inputEmail, labelEmail) {
+  function inputEmailOrInputPasswordTabKeyFocusedOn(
+    inputEmail,
+    labelEmail,
+    inputPassword,
+    labelPassword
+  ) {
     window.onkeyup =
       /**
        *
@@ -115,18 +134,24 @@
        */
       function (event) {
         const { key } = event;
-        console.log(1);
+
         if (wasTabKey(key)) {
           const tabKeyFocusedElement = window.document.activeElement;
           const className = tabKeyFocusedElement.classList[0];
-
           if (wasInputEmailString(className)) {
-            addCSSForInputEmailWhenItFocused(inputEmail, labelEmail);
-            return;
+            addCSSForInputWhenItFocused(inputEmail, labelEmail);
           }
 
           if (!wasInputEmailString(className)) {
-            removeCSSForInputEmailWhenItFocused(inputEmail, labelEmail);
+            removeCSSForInputWhenItFocused(inputEmail, labelEmail);
+          }
+
+          if (wasInputPasswordString(className)) {
+            addCSSForInputWhenItFocused(inputPassword, labelPassword);
+          }
+
+          if (!wasInputPasswordString(className)) {
+            removeCSSForInputWhenItFocused(inputPassword, labelPassword);
           }
         }
       };
@@ -135,26 +160,118 @@
   /**
    *
    * @param {HTMLInputElement} inputEmail
+   * @param {number} x
+   * @param {number} y
+   *
+   *
+   */
+  function detectInputEmailArea(inputEmail, x, y) {
+    const offsetTop = inputEmail.getBoundingClientRect().top;
+    const offsetLeft = inputEmail.getBoundingClientRect().left;
+
+    const height = inputEmail.getBoundingClientRect().height;
+    const width = inputEmail.getBoundingClientRect().width;
+
+    const maxYRange = offsetTop + height - 2;
+    const maxXRange = offsetLeft + width;
+
+    const f1 = y >= offsetTop && y <= maxYRange;
+    const f2 = x >= offsetLeft && x <= maxXRange;
+    const f3 = f1 && f2;
+
+    return f3;
+  }
+
+  /**
+   *
+   * @param {HTMLInputElement} inputPassword
+   * @param {number} x
+   * @param {number} y
+   *
+   */
+  function detectInputPasswordArea(inputPassword, x, y) {
+    const offsetTop = inputPassword.getBoundingClientRect().top;
+    const offsetLeft = inputPassword.getBoundingClientRect().left;
+
+    const width = inputPassword.getBoundingClientRect().width;
+    const height = inputPassword.getBoundingClientRect().height;
+
+    const delta1 = offsetLeft + width;
+    const delta2 = offsetTop + height - 2;
+
+    const flag1 = x >= offsetLeft && x <= delta1;
+    const flag2 = y >= offsetTop && x <= delta2;
+    const flag3 = flag1 && flag2;
+
+    if (flag3) {
+      return;
+    }
+  }
+
+  /**
+   *
+   * @param {HTMLInputElement} inputEmail
+   * @param {HTMLLabelElement} labelEmail
+   * @param {HTMLLabelElement} inputPassword
+   * @param {HTMLLabelElement} labelPassword
+   *
+   *
+   */
+
+  function inputEmailOrInputPasswordMouseClickedOn(
+    inputEmail,
+    labelEmail,
+    inputPassword,
+    labelPassword
+  ) {
+    window.onmouseup =
+      /**
+       *
+       * @param {MouseEvent} event
+       */
+      function (event) {
+        const { pageX: x, pageY: y } = event;
+
+        const wasInputEmailArea = detectInputEmailArea(inputEmail, x, y);
+        const wasInputPasswordArea = detectInputEmailArea(inputPassword, x, y);
+
+        if (wasInputEmailArea) {
+          addCSSForInputWhenItFocused(inputEmail, labelEmail);
+        }
+
+        if (!wasInputEmailArea) {
+          removeCSSForInputWhenItFocused(inputEmail, labelEmail);
+        }
+
+        if (wasInputPasswordArea) {
+          addCSSForInputWhenItFocused(inputPassword, labelPassword);
+        }
+
+        if (!wasInputPasswordArea) {
+          removeCSSForInputWhenItFocused(inputPassword, labelPassword);
+        }
+      };
+  }
+
+  /**
+   *
+   * @param {HTMLInputElement} inputEmail
    * @param {HTMLElement} labelEmail
-   * @param {HTMLElement} pseudoBorderBottomOnFocus
    *
    *
    *
    */
-  function inputEmailOnfocus(
-    inputEmail,
-    labelEmail,
-    pseudoBorderBottomOnFocus
-  ) {
+  function inputEmailOnfocus(inputEmail, labelEmail) {
     /**
      * Problem: Detect Toggle Input Email On Focus
      * + The Input Email Focused When
-     *   + The mouse clicked on
-     *   + The Tab key focus on
+     *   + The mouse clicked on - done
+     *   + The Tab key focus on - done
      *
      */
 
-    inputEmailTabKeyFocusedOn(inputEmail, labelEmail);
+    inputEmailOrInputPasswordTabKeyFocusedOn(inputEmail, labelEmail);
+    inputEmailOrInputPasswordMouseClickedOn(inputEmail, labelEmail);
   }
 
   function detectDOMChange() {
@@ -173,6 +290,56 @@
     });
   }
 
+  /**
+   *
+   * @param {HTMLInputElement} inputPassword
+   * @param {HTMLInputElement} labelPassword
+   *
+   */
+  function tabKeyFocusedOnInputPassword(inputPassword, labelPassword) {
+    window.onkeyup = function (event) {
+      const { key } = event;
+
+      if (wasTabKey(key)) {
+        return;
+      }
+
+      if (!wasTabKey(key)) {
+      }
+    };
+  }
+
+  /**
+   *
+   * @param {HTMLInputElement} inputEmail
+   * @param {HTMLInputElement} labelEmail
+   * @param {HTMLInputElement} inputPassword
+   * @param {HTMLInputElement} labelPassword
+   *
+   *
+   */
+  function inputEmailOrInputPasswordOnFocus(
+    inputEmail,
+    labelEmail,
+    inputPassword,
+    labelPassword
+  ) {
+    // input email or input password on focus
+    inputEmailOrInputPasswordTabKeyFocusedOn(
+      inputEmail,
+      labelEmail,
+      inputPassword,
+      labelPassword
+    );
+
+    inputEmailOrInputPasswordMouseClickedOn(
+      inputEmail,
+      labelEmail,
+      inputPassword,
+      labelPassword
+    );
+  }
+
   async function main() {
     const layer = window.document.getElementsByClassName(classes.layer)[0];
     await asyncTitleFormOnload(layer);
@@ -184,11 +351,28 @@
     const labelEmail = window.document.getElementsByClassName(
       classes.labelEmail
     )[0];
+
+    const inputPassword = window.document.getElementsByClassName(
+      classes.inputPassword
+    )[0];
+    const labelPassword = window.document.getElementsByClassName(
+      classes.labelPassword
+    )[0];
+
     const pseudoBorderBottomOnFocus = window.document.getElementsByClassName(
       classes.pseudoBorderBottomOnFocus
     )[0];
 
-    inputEmailOnfocus(inputEmail, labelEmail, pseudoBorderBottomOnFocus);
+    // inputEmailOnfocus(inputEmail, labelEmail);
+
+    // inputPasswordOnFocus(inputPassword, labelPassword);
+
+    inputEmailOrInputPasswordOnFocus(
+      inputEmail,
+      labelEmail,
+      inputPassword,
+      labelPassword
+    );
   }
 
   main();
