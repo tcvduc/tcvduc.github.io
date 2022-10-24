75598baeb279b0f2adcf4ecc7c82564710ed3af3
tcvduc <caovanducs@gmail.com>
2022-10-24 21:27:28 +0700

    train

--- portfolio-shred/14.navigation-animation/train/solve-1/14.js
+++ portfolio-shred/14.navigation-animation/train/solve-1/14.js
@@ -2,15 +2,30 @@
   const classes = {
     active: "active",
     navigation: "navigation",
+    listLink: "listLink",
+    displayNone: "displayNone",
+    svgNavigationIcon: "svgNavigationIcon",
+    line1: "line1",
+    line2: "line2",
   };
 
   /**
    *
    * @param {HTMLElement} navigation
+   * @param {HTMLElement} listLink
+   * @param {HTMLElement} line1
+   * @param {HTMLElement} line2
+   *
+   *
+   *
    */
-  function navigationOnclick(navigation) {
+  function navigationOnclick(navigation, listLink, line1, line2) {
     navigation.onclick = function () {
-      navigation.classList.toggle(classes.active);
+      line1.classList.toggle(classes.active);
+      line2.classList.toggle(classes.active);
+
+      // navigation.classList.toggle(classes.active);
+      // listLink.classList.toggle(classes.displayNone);
     };
   }
 
@@ -19,7 +34,14 @@
       classes.navigation
     )[0];
 
-    navigationOnclick(navigation);
+    const listLink = window.document.getElementsByClassName(
+      classes.listLink
+    )[0];
+
+    const line1 = window.document.getElementsByClassName(classes.line1)[0];
+    const line2 = window.document.getElementsByClassName(classes.line2)[0];
+
+    navigationOnclick(navigation, listLink, line1, line2);
   }
 
   main();
