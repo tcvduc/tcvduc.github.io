6d9a30c8511293d7568fc6fa8befbd3883e35f99
tcvduc <caovanducs@gmail.com>
2022-11-02 13:19:49 +0700

    add log for test protect branch purpose

--- train.js
+++ train.js
@@ -1,10 +1,1 @@
-const object = {
-  key1: "value1",
-  key2: "value2",
-  key3: "value3",
-};
-
-const hasOwnProperty = Object.prototype.hasOwnProperty;
-
-console.log(hasOwnProperty.call(object, "key1"));
-console.log(hasOwnProperty.call(object, "key4"));
+console.log(1);
