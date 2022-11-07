3c2ab61bf9e3064927b05df295f83ceabca9cdd3
tcvduc <caovanducs@gmail.com>
2022-11-07 13:42:38 +0700

    train 17

--- portfolio-shred/17.movie-app/train/train-1/17.js
+++ portfolio-shred/17.movie-app/train/train-1/17.js
@@ -2,6 +2,8 @@
   const API_URL =
     "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
   const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+  const SEARCH_API =
+    'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
   const ids = {
     main: "main",
@@ -51,11 +53,15 @@
         movieElement.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt ="${title}"  />
 
-            <div class=${classes.movieInfo} >
+            <div class=${classes.movieInfo}>
                 <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}" >
+                <span class="${getClassByRate(vote_average)}">
                     ${vote_average}
                 </span>
+            </div>
+
+            <div class="overview">
+              ${overview}
             </div>
         `;
 
@@ -80,6 +86,38 @@
     return classes.red;
   }
 
+  /**
+   *
+   * @param {HTMLFormElement} formElement
+   * @param {HTMLInputElement} searchElement
+   * @param {HTMLElement} mainElement
+   *
+   *
+   *
+   */
+
+  function formElementOnsubmit(formElement, searchElement, mainElement) {
+    formElement.onsubmit =
+      /**
+       *
+       * @param {SubmitEvent} event
+       */
+      async function (event) {
+        event.preventDefault();
+
+        const searchTerm = searchElement.value;
+
+        if (searchTerm && searchTerm !== "") {
+          const movies = await getMovies(SEARCH_API + searchTerm);
+          showMovies(movies, mainElement);
+
+          searchElement.value = "";
+        } else {
+          window.location.reload();
+        }
+      };
+  }
+
   async function main() {
     const mainElement = window.document.getElementById(ids.main);
     const formElement = window.document.getElementById(ids.form);
@@ -88,6 +126,7 @@
 
     window.console.table([{ movies: movies }]);
     showMovies(movies, mainElement);
+    formElementOnsubmit(formElement, searchElement, mainElement);
   }
 
   main();
