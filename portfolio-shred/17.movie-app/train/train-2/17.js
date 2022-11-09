0d79d5887191d4ac897e14ef9d30189e145cee76
tcvduc <caovanducs@gmail.com>
2022-11-09 14:43:03 +0700

    train 17

--- portfolio-shred/17.movie-app/train/train-2/17.js
+++ portfolio-shred/17.movie-app/train/train-2/17.js
@@ -5,12 +5,16 @@
     film: "film",
   };
 
-  const Film = {
-    filmPoster: "filmPoster",
-    filmTitle: "filmTitle",
-    overview: "overview",
-    filmRate: "filmRate",
-  };
+  const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+  class Film {
+    constructor(filmPoster, filmTitle, overview, filmRate) {
+      this.filmPoster = filmPoster;
+      this.filmTitle = filmTitle;
+      this.overview = overview;
+      this.filmRate = filmRate;
+    }
+  }
 
   /**
    *
@@ -57,14 +61,22 @@
    */
   function showFilms(films, layerFilms) {
     films.forEach(function (film) {
-      console.log(film);
       const {
         poster_path: filmPoster,
         original_title: filmTitle,
-        overview,
+        overview: overview,
         vote_average: filmRate,
       } = film;
-      const filmElement = createAnFilmElement(film);
+      console.log("filmPoster: ", filmPoster);
+
+      const filmObject = new Film(
+        IMG_PATH + filmPoster,
+        filmTitle,
+        overview,
+        filmRate
+      );
+
+      const filmElement = createAnFilmElement(filmObject);
       layerFilms.appendChild(filmElement);
     });
   }
@@ -80,7 +92,6 @@
       "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c";
     const pageQuery = "&page=1";
 
-    const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
     const SEARCH_API =
       'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
