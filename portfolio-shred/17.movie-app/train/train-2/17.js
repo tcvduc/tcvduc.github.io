e32a9728ed8740dd45c45b75d9dc448fd9842653
tcvduc <caovanducs@gmail.com>
2022-11-19 13:43:08 +0700

    train

--- portfolio-shred/17.movie-app/train/train-2/17.js
+++ portfolio-shred/17.movie-app/train/train-2/17.js
@@ -3,9 +3,19 @@
     searchInput: "searchInput",
     layerFilms: "layerFilms",
     film: "film",
+    active: "active",
+  };
+
+  const paginationElements = {
+    pagination: "pagination",
+    previousButton: "previous-button",
+    pageButtonList: "page-button-list",
+    page: "page",
+    nextButton: "next-button",
   };
 
   const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+  const LAYER_PAGINATION_MAX_WIDTH = 600;
 
   class Film {
     constructor(filmPoster, filmTitle, overview, filmRate) {
@@ -60,6 +70,8 @@
    *
    */
   function showFilms(films, layerFilms) {
+    layerFilms.innerHTML = "";
+
     films.forEach(function (film) {
       const {
         poster_path: filmPoster,
@@ -67,7 +79,6 @@
         overview: overview,
         vote_average: filmRate,
       } = film;
-      console.log("filmPoster: ", filmPoster);
 
       const filmObject = new Film(
         IMG_PATH + filmPoster,
@@ -79,6 +90,119 @@
       const filmElement = createAnFilmElement(filmObject);
       layerFilms.appendChild(filmElement);
     });
+  }
+
+  /**
+   *
+   * @param {number} page
+   */
+  function createPageButton(page) {
+    const element = window.document.createElement("page");
+    element.innerHTML = page;
+    return element;
+  }
+
+  function getTotalPageNumberThatShouldBeDisplayed() {
+    const deviceWidth = window.innerWidth;
+    const previousButtonWidth = 40;
+    const nextButtonWidth = 40;
+    const gap = 12;
+    const pageButtonWidth = 40;
+
+    if (deviceWidth < LAYER_PAGINATION_MAX_WIDTH) {
+      const pagination = window.document.getElementsByTagName(
+        paginationElements.pagination
+      )[0];
+      const paginationWidth = pagination.getBoundingClientRect().width;
+
+      let widthIncreaser = previousButtonWidth + nextButtonWidth + gap + gap;
+      let totalPageNumber = 0;
+
+      for (let i = 1; i <= 100; ++i) {
+        widthIncreaser += gap + pageButtonWidth;
+        totalPageNumber++;
+
+        const delta = paginationWidth - widthIncreaser;
+        if (delta <= 0) {
+          break;
+        }
+      }
+      return totalPageNumber;
+    }
+
+    if (deviceWidth >= LAYER_PAGINATION_MAX_WIDTH) {
+      let widthIncreaser = previousButtonWidth + nextButtonWidth + gap + gap;
+      let totalPageNumber = 0;
+
+      for (let i = 1; i <= 100; ++i) {
+        widthIncreaser += gap + pageButtonWidth;
+
+        if (widthIncreaser >= LAYER_PAGINATION_MAX_WIDTH) {
+          totalPageNumber--;
+          break;
+        }
+
+        totalPageNumber++;
+      }
+
+      return totalPageNumber;
+    }
+  }
+
+  /**
+   *
+   * @param {HTMLElement} pageButtonList
+   */
+  function displayPagination(pageButtonList) {
+    /**
+     * Problem: Display Pagination
+     * + Calculate total page number suitable to display
+     * on the UI - done
+     * + display page button - done
+     *
+     */
+    const totalPageNumber = getTotalPageNumberThatShouldBeDisplayed();
+
+    for (let i = 1; i <= totalPageNumber; ++i) {
+      const page = createPageButton(i);
+
+      if (i === 1) {
+        page.classList.add(classes.active);
+      }
+
+      pageButtonList.appendChild(page);
+    }
+
+    const pages = window.document.getElementsByTagName(paginationElements.page);
+    paginationOnclick(pages);
+  }
+
+  /**
+   *
+   * @param {HTMLElement[]} pages
+   */
+  function paginationOnclick(pages) {
+    const API_URL =
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c";
+    const layerFilms = window.document.getElementsByClassName(
+      classes.layerFilms
+    )[0];
+
+    for (let i = pages.length - 1; i >= 0; --i) {
+      pages[i].onclick = async function () {
+        for (let j = pages.length - 1; j >= 0; --j) {
+          pages[j].classList.remove(classes.active);
+        }
+
+        pages[i].classList.add(classes.active);
+        const page = +pages[i].textContent;
+
+        const url = API_URL + `&page=${page}`;
+        const fetchFilms = await getFilms(url);
+        const films = fetchFilms.results;
+        showFilms(films, layerFilms);
+      };
+    }
   }
 
   async function main() {
@@ -96,9 +220,15 @@
       'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
     const url = API_URL + pageQuery;
-    const filmsWithPagi = await getFilms(url);
-    const films = filmsWithPagi.results;
+    const filmsWithPageQuery = await getFilms(url);
+
+    const films = filmsWithPageQuery.results;
+    const pageButtonList = window.document.getElementsByTagName(
+      paginationElements.pageButtonList
+    )[0];
+
     showFilms(films, layerFilms);
+    displayPagination(pageButtonList);
   }
 
   main();
