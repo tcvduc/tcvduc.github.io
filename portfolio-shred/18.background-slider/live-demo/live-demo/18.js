3793962ace6ba5264daf446b0dbab9d147223d52
tcvduc <caovanducs@gmail.com>
2022-11-27 11:53:04 +0700

    train

--- portfolio-shred/18.background-slider/live-demo/live-demo/18.js
+++ portfolio-shred/18.background-slider/live-demo/live-demo/18.js
@@ -1,117 +1,191 @@
 (function () {
   const classes = {
-    layer1: "layer1",
-    layer3: "layer3",
+    leftArrow: "left-arrow",
+    rightArrow: "right-arrow",
+    slide: "slide",
     active: "active",
-    leftArrow: "leftArrow",
-    rightArrow: "rightArrow",
+    slideContainer: "slider-container",
   };
 
-  const images = [
-    "https://images.hdqwalls.com/download/small-memory-8k-2a-7680x4320.jpg",
-    "https://images.hdqwalls.com/download/small-memory-evening-8k-5o-7680x4320.jpg",
-    "https://images8.alphacoders.com/903/903481.jpg",
-    "https://c4.wallpaperflare.com/wallpaper/664/373/122/mikael-gustafsson-artwork-horizon-fox-wallpaper-preview.jpg",
-    "https://p4.wallpaperbetter.com/wallpaper/566/335/681/forest-firewatch-nature-digital-art-wallpaper-preview.jpg",
-  ];
-  let imageIndex = 0;
-  const imageMaximumIndex = images.length - 1;
-  const scaleY = 1.07;
+  let index = 0;
 
-  /**
-   *
-   * @param {number} n
-   */
-  function wasOddNumber(n) {
-    /**
-     * + Odd Number: 1, 3, 5, 7,..., 2k + 1
-     * + Odd number is not divided by 2
-     *
-     *
-     */
-    return n % 2 !== 0;
-  }
+  const keyObjectFunctions = {
+    ArrowLeft:
+      /**
+       *
+       * @param {HTMLElement[]} slides
+       */
+      function (slides) {
+        index--;
+
+        if (index < 0) {
+          index = slides.length - 1;
+        }
+
+        for (let j = slides.length - 1; j >= 0; --j) {
+          slides[j].classList.remove(classes.active);
+        }
+
+        slides[index].classList.add(classes.active);
+
+        window.document.body.style.backgroundImage =
+          slides[index].style.backgroundImage;
+      },
+
+    ArrowRight:
+      /**
+       *
+       * @param {HTMLElement[]} slides
+       */
+      function (slides) {
+        index++;
+
+        if (index > slides.length - 1) {
+          index = 0;
+        }
+
+        for (let k = slides.length - 1; k >= 0; --k) {
+          slides[k].classList.remove(classes.active);
+        }
+
+        slides[index].classList.add(classes.active);
+
+        window.document.body.style.backgroundImage =
+          slides[index].style.backgroundImage;
+      },
+
+    ArrowUp:
+      /**
+       *
+       * @param {HTMLElement[]} slides
+       */
+      function (slides) {
+        index++;
+
+        if (index > slides.length - 1) {
+          index = 0;
+        }
+
+        for (let i = slides.length - 1; i >= 0; --i) {
+          slides[i].classList.remove(classes.active);
+        }
+
+        slides[index].classList.add(classes.active);
+
+        window.document.body.style.backgroundImage =
+          slides[index].style.backgroundImage;
+      },
+
+    ArrowDown:
+      /**
+       *
+       * @param {HTMLElement[]} slides
+       */
+      function (slides) {
+        index--;
+
+        if (index < 0) {
+          index = slides.length - 1;
+        }
+
+        for (let j = slides.length - 1; j >= 0; --j) {
+          slides[j].classList.remove(classes.active);
+        }
+
+        slides[index].classList.add(classes.active);
+
+        window.document.body.style.backgroundImage =
+          slides[index].style.backgroundImage;
+      },
+  };
 
   /**
    *
    * @param {HTMLElement} leftArrow
-   * @param {HTMLElement} layer1
-   * @param {HTMLElement} layer3
+   * @param {HTMLElement[]} slides
+   * @param {HTMLElement} body
+   *
    *
    */
-  function leftArrowOnclick(leftArrow, layer1, layer3) {
+  function leftArrowOnclick(leftArrow, slides, body) {
     leftArrow.onclick = function () {
-      imageIndex--;
+      index--;
 
-      if (imageIndex < 0) {
-        imageIndex = imageMaximumIndex;
+      if (index < 0) {
+        index = slides.length - 1;
       }
 
-      if (wasOddNumber(imageIndex)) {
-        layer1.style.backgroundImage = `url('${images[imageIndex]}')`;
-        layer1.style.transform = `scaleY(${scaleY})`;
-        layer3.style.backgroundImage = `url('${images[imageIndex]}')`;
-
-        return;
+      for (let i = slides.length - 1; i >= 0; --i) {
+        slides[i].classList.remove(classes.active);
       }
 
-      if (!wasOddNumber(imageIndex)) {
-        layer1.style.backgroundImage = `url('${images[imageIndex]}')`;
-        layer1.style.transform = "scale(1)";
+      slides[index].classList.add(classes.active);
 
-        layer3.style.backgroundImage = `url('${images[imageIndex]}')`;
-      }
+      body.style.backgroundImage = slides[index].style.backgroundImage;
     };
   }
 
   /**
    *
    * @param {HTMLElement} rightArrow
-   * @param {HTMLElement} layer1
-   * @param {HTMLElement} layer3
+   * @param {HTMLElement[]} slides
+   * @param {HTMLElement} body
    *
    */
-  function rightArrowOnclick(rightArrow, layer1, layer3) {
+  function rightArrowOnclick(rightArrow, slides, body) {
     rightArrow.onclick = function () {
-      imageIndex++;
+      index++;
 
-      if (imageIndex > images.length - 1) {
-        imageIndex = 0;
+      if (index > slides.length - 1) {
+        index = 0;
       }
 
-      if (wasOddNumber(imageIndex)) {
-        layer1.style.backgroundImage = `url('${images[imageIndex]}')`;
-        layer1.style.transform = `scaleY(${scaleY})`;
-
-        layer3.style.backgroundImage = `url('${images[imageIndex]}')`;
-
-        return;
+      for (let i = slides.length - 1; i >= 0; --i) {
+        slides[i].classList.remove(classes.active);
       }
 
-      if (!wasOddNumber(imageIndex)) {
-        layer1.style.backgroundImage = `url('${images[imageIndex]}')`;
-        layer1.style.transform = `scaleY(1)`;
+      slides[index].classList.add(classes.active);
 
-        layer3.style.backgroundImage = `url('${images[imageIndex]}')`;
-      }
+      body.style.backgroundImage = slides[index].style.backgroundImage;
     };
   }
 
+  /**
+   *
+   * @param {HTMLElement[]} slides
+   */
+  function detectKeypress(slides) {
+    window.onkeyup =
+      /**
+       *
+       * @param {KeyboardEvent} event
+       */
+      function (event) {
+        const { key } = event;
+
+        if (keyObjectFunctions[key]) {
+          keyObjectFunctions[key](slides);
+        }
+      };
+  }
+
   function main() {
-    const layer1 = window.document.getElementsByClassName(classes.layer1)[0];
-    const layer3 = window.document.getElementsByClassName(classes.layer3)[0];
     const leftArrow = window.document.getElementsByClassName(
       classes.leftArrow
     )[0];
     const rightArrow = window.document.getElementsByClassName(
       classes.rightArrow
     )[0];
+    const slides = window.document.getElementsByClassName(classes.slide);
+    const slideContainer = window.document.getElementsByClassName(
+      classes.slideContainer
+    )[0];
+    const body = window.document.body;
 
-    layer1.style.backgroundImage = `url('${images[imageIndex]}')`;
-    layer3.style.backgroundImage = `url('${images[imageIndex]}')`;
+    leftArrowOnclick(leftArrow, slides, body);
+    rightArrowOnclick(rightArrow, slides, body);
 
-    leftArrowOnclick(leftArrow, layer1, layer3);
-    rightArrowOnclick(rightArrow, layer1, layer3);
+    detectKeypress(slides);
   }
 
   main();
