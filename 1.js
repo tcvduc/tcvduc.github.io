01a3f2db4318c6e32a255becb2db69d4ad43261e
tcvduc <caovanducs@gmail.com>
2022-12-04 14:01:51 +0700

    qwerty

--- 1.js
+++ 1.js
@@ -1,9 +1,133 @@
-const keys = {
-  ArrowDown: "ArrowDown Object value",
-};
-const keyArray = ["ArrowDown", "ArrowUp"];
+/**
+ *
+ * @param {Array} a
+ * @param {any} element
+ */
+function push(a, element) {
+  /**
+   * + a = [1,2,3,4,5]
+   * + element = 6
+   * + ret = [1,2,3,4,5,6]
+   *
+   */
+  const ret = new Array(a.length + 1);
 
-const string = "ArrowDown";
+  for (let i = ret.length - 1 - 1; i >= 0; --i) {
+    ret[i] = a[i];
+  }
 
-console.log(keys[string]);
-console.log(keyArray[string]);
+  ret[ret.length - 1] = element;
+
+  return ret;
+}
+
+/**
+ *
+ * @param {Array} a
+ */
+function reverseArray(a) {
+  /**
+   * -------0 1 2
+   * + a = [1,2,3]
+   *
+   * ---------0 1 2
+   * + ret = [3,2,1]
+   *
+   * + ret[0] = a[2]
+   * + ret[1] = a[1]
+   * + ret[2] = a[0]
+   *
+   *
+   * + i: length -> 0
+   * + ret[i] = a[length - i - 1]
+   */
+
+  const length = a.length;
+  const ret = new Array(length);
+
+  for (let i = length - 1; i >= 0; --i) {
+    ret[i] = a[length - i - 1];
+  }
+
+  return ret;
+}
+
+/**
+ *
+ * @param {Array} a
+ */
+function pop(a) {
+  /**
+   * -------0 1 2 3
+   * + a = [1,2,3,4]
+   * ---------0 1 2
+   * + ret = [1,2,3]
+   *
+   */
+  const ret = new Array(a.length - 1);
+
+  for (let i = ret.length - 1; i >= 0; --i) {
+    ret[i] = a[i];
+  }
+
+  return ret;
+}
+
+/**
+ *
+ * @param {Array} a
+ * @param {string} factor
+ */
+function join(a, factor) {
+  /**
+   * + a = ['1','0']
+   * + factor = '-'
+   * + ret = "1-0"
+   *
+   */
+  let ret = "";
+
+  for (let i = 0; i <= a.length - 1; ++i) {
+    if (i === a.length - 1) {
+      ret += a[i];
+      break;
+    }
+
+    ret += a[i] + factor;
+  }
+
+  return ret;
+}
+
+/**
+ *
+ * @param {string} pixel
+ */
+function getPixelNumber(pixel) {
+  /**
+   * --0123
+   * + 10px
+   * + length: 4
+   * + traverse to 1:
+   *   + 1 = 4 - 1 - 1 - 1
+   *   + 1 = length - 1 - 1 -1
+   * + ret: 10
+   *
+   */
+  let ret = [];
+  const length = pixel.length;
+
+  for (let i = length - 1 - 1 - 1; i >= 0; --i) {
+    ret = push(ret, pixel[i]);
+  }
+
+  ret = reverseArray(ret);
+
+  ret = join(ret, "");
+
+  return +ret;
+}
+
+const pixel = "12345px";
+
+console.log(getPixelNumber(pixel)); // 10
--- portfolio-shred/19.theme-clock/train/train-1/19.js
+++ portfolio-shred/19.theme-clock/train/train-1/19.js
@@ -76,8 +76,188 @@
         hourTimeUnit = 0;
       }
 
-      console.log(`${hourTimeUnit}:${minuteTimeUnit}:${secondTimeUnit}`);
+      // console.log(`${hourTimeUnit}:${minuteTimeUnit}:${secondTimeUnit}`);
     }, oneSecondIntervalUnitFastDebug);
+  }
+  /**
+   *
+   * @param {Array} a
+   * @param {any} element
+   */
+  function push(a, element) {
+    /**
+     * + a = [1,2,3,4,5]
+     * + element = 6
+     * + ret = [1,2,3,4,5,6]
+     *
+     */
+    const ret = new Array(a.length + 1);
+
+    for (let i = ret.length - 1 - 1; i >= 0; --i) {
+      ret[i] = a[i];
+    }
+
+    ret[ret.length - 1] = element;
+
+    return ret;
+  }
+
+  /**
+   *
+   * @param {Array} a
+   */
+  function reverseArray(a) {
+    /**
+     * -------0 1 2
+     * + a = [1,2,3]
+     *
+     * ---------0 1 2
+     * + ret = [3,2,1]
+     *
+     * + ret[0] = a[2]
+     * + ret[1] = a[1]
+     * + ret[2] = a[0]
+     *
+     *
+     * + i: length -> 0
+     * + ret[i] = a[length - i - 1]
+     */
+
+    const length = a.length;
+    const ret = new Array(length);
+
+    for (let i = length - 1; i >= 0; --i) {
+      ret[i] = a[length - i - 1];
+    }
+
+    return ret;
+  }
+
+  /**
+   *
+   * @param {Array} a
+   */
+  function pop(a) {
+    /**
+     * -------0 1 2 3
+     * + a = [1,2,3,4]
+     * ---------0 1 2
+     * + ret = [1,2,3]
+     *
+     */
+    const ret = new Array(a.length - 1);
+
+    for (let i = ret.length - 1; i >= 0; --i) {
+      ret[i] = a[i];
+    }
+
+    return ret;
+  }
+
+  /**
+   *
+   * @param {Array} a
+   * @param {string} factor
+   */
+  function join(a, factor) {
+    /**
+     * + a = ['1','0']
+     * + factor = '-'
+     * + ret = "1-0"
+     *
+     */
+    let ret = "";
+
+    for (let i = 0; i <= a.length - 1; ++i) {
+      if (i === a.length - 1) {
+        ret += a[i];
+        break;
+      }
+
+      ret += a[i] + factor;
+    }
+
+    return ret;
+  }
+
+  /**
+   *
+   * @param {string} pixel
+   */
+  function getPixelNumber(pixel) {
+    /**
+     * --0123
+     * + 10px
+     * + length: 4
+     * + traverse to 1:
+     *   + 1 = 4 - 1 - 1 - 1
+     *   + 1 = length - 1 - 1 -1
+     * + ret: 10
+     *
+     */
+    let ret = [];
+    const length = pixel.length;
+
+    for (let i = length - 1 - 1 - 1; i >= 0; --i) {
+      ret = push(ret, pixel[i]);
+    }
+
+    ret = reverseArray(ret);
+
+    ret = join(ret, "");
+
+    return +ret;
+  }
+
+  /**
+   *
+   * @param {HTMLElement} element
+   */
+  function dragElement(element) {
+    let x1 = 0;
+    let x2 = 0;
+    let y1 = 0;
+    let y2 = 0;
+
+    element.onmousedown =
+      /**
+       *
+       * @param {MouseEvent} event
+       */
+      function (event) {
+        const { pageX, pageY } = event;
+        x1 = pageX;
+        y1 = pageY;
+
+        console.log("mouse down");
+
+        element.onmousemove =
+          /**
+           *
+           * @param {MouseEvent} event
+           */
+          function (event) {
+            const { pageX, pageY } = event;
+            x2 = pageX;
+            y2 = pageY;
+
+            const deltaX = x2 - x1;
+            const elementLeftNumber = getPixelNumber(element.style.left);
+            console.log(`elementLeftNumber:${elementLeftNumber}`);
+
+            element.style.left = `${elementLeftNumber + deltaX}px`;
+          };
+
+        element.onmouseup =
+          /**
+           *
+           * @param {MouseEvent} event
+           */
+          function (event) {
+            element.onmousemove = null;
+            element.onmousedown = null;
+          };
+      };
   }
 
   function main() {
@@ -94,6 +274,8 @@
 
     clockOnclick(clock, barMinute, centerPoint);
     barSecondClockWiseRotateActive(barSecond);
+
+    dragElement(barMinute);
   }
 
   main();
