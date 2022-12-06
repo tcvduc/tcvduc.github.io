f0505252d0f2718c6d1aa03aa35eba4a86457a8c
tcvduc <caovanducs@gmail.com>
2022-12-06 13:32:05 +0700

    train

--- portfolio-shred/19.theme-clock/train/train-1/19.js
+++ portfolio-shred/19.theme-clock/train/train-1/19.js
@@ -6,6 +6,8 @@
     active: "active",
     clock: "clock",
     centerPoint: "centerPoint",
+    circleBarMinute: "circleBarMinute",
+    circleBarHour: "circleBarHour",
   };
 
   const maxSecondTimeUnit = 60;
@@ -42,9 +44,48 @@
    *
    */
   function clockOnclick(clock, barMinute, centerPoint) {
-    clock.onclick = function () {
-      toggleElementClassName(centerPoint, classes.active);
-    };
+    if (clock) {
+      clock.onclick = function () {
+        if (centerPoint) {
+          toggleElementClassName(centerPoint, classes.active);
+        }
+      };
+    }
+  }
+
+  function debugSecondMatchDegree() {
+    /**
+     * + Second: 1 -> 5
+     *   + Second Minimum: 1
+     *   + Second Maximum: 5
+     * + Degree: 1 -> 10
+     *   + Degree Minimum: 1
+     *   + Degree Maximum: 10
+     * + The Equation
+     *   + delta = degree maximum / second maximum
+     *   + degree increasing = second increasing x delta
+     *
+     * + step 1: increase the second from 1 to 5 - done
+     * + step 2: use the equation increase the degree - done
+     * + step 3: watch ret - done
+     *
+     *
+     */
+    const secondMaximum = 5;
+    const degreeMaximum = 10;
+    const delta = degreeMaximum / secondMaximum;
+    let secondIncrease = 1;
+    let secondIntervalTimeUnit = 1000;
+    let degreeIncrease = 0;
+
+    const interval = window.setInterval(function () {
+      console.log(`${secondIncrease} - ${degreeIncrease}`);
+      if (secondIncrease === secondMaximum) {
+        return window.clearInterval(interval);
+      }
+      secondIncrease++;
+      degreeIncrease = secondIncrease * delta;
+    }, secondIntervalTimeUnit);
   }
 
   /**
@@ -52,32 +93,88 @@
    * @param {HTMLElement} barSecond
    */
   function barSecondClockWiseRotateActive(barSecond) {
-    let oneSecondIntervalUnit = 1000;
-    let oneSecondIntervalUnitFastDebug = 1;
+    if (barSecond) {
+      /**
+       * Problem: Second Bar Rotate Clock Wise
+       *
+       * + Second: 0 -> 59
+       *   + second minimum: 0
+       *   + second maximum: 59
+       * + Degree: 0 -> 359
+       *   + degree minimum: 0
+       *   + degree maximum: 359
+       * + The Equation
+       *   + delta = degree maximum / second maximum
+       *   + degree increasing = second increasing x delta
+       *
+       *
+       * -> How to make second and degree match?
+       * + Second: 1 -> 5
+       *   + Second Minimum: 1
+       *   + Second Maximum: 5
+       * + Degree: 1 -> 10
+       *   + Degree Minimum: 1
+       *   + Degree Maximum: 10
+       *
+       * + Second: 1
+       *   + Degree: 2 = second x 2
+       *     + second: 1
+       *     + 2: Degree Maximum / Second Maximum
+       * + Second: 2
+       *   + Degree: 4 = second x 2
+       *     + second: 2
+       *     + 2: Degree Maximum / Second Maximum
+       * + Second: 3
+       *   + Degree: 6 = second x 2
+       * + Second: 4
+       *   + Degree: 8 = second x 2
+       * + Second: 5
+       *   + Degree: 10 = second x 2
+       *
+       *
+       *
+       * -> delta = Degree Maximum / Second Maximum
+       *
+       * The Equation
+       * + delta = Degree Maximum / Second Maximum
+       * + DegreeIncreasing = SecondIncreasing x delta
+       *
+       *
+       *
+       *
+       *
+       */
+      debugSecondMatchDegree();
 
-    let secondTimeUnit = 0;
-    let minuteTimeUnit = 0;
-    let hourTimeUnit = 0;
+      let oneSecondIntervalUnit = 1000;
+      let oneSecondIntervalUnitFastDebug = 100;
 
-    const timeInterval = window.setInterval(function () {
-      secondTimeUnit++;
-      oneSecondIntervalUnit += 1000;
-      if (secondTimeUnit === maxSecondTimeUnit) {
-        secondTimeUnit = 0;
-        minuteTimeUnit++;
-      }
+      let secondTimeUnit = 0;
+      let minuteTimeUnit = 58;
+      let hourTimeUnit = 22;
 
-      if (minuteTimeUnit === maxMinuteTimeUnit) {
-        minuteTimeUnit = 0;
-        hourTimeUnit++;
-      }
+      const timeInterval = window.setInterval(function () {
+        secondTimeUnit++;
+        oneSecondIntervalUnit += 1000;
+        if (secondTimeUnit === maxSecondTimeUnit) {
+          secondTimeUnit = 0;
+          minuteTimeUnit++;
+        }
 
-      if (hourTimeUnit === maxHourTimeUnit) {
-        hourTimeUnit = 0;
-      }
+        if (minuteTimeUnit === maxMinuteTimeUnit) {
+          minuteTimeUnit = 0;
+          hourTimeUnit++;
+        }
 
-      // console.log(`${hourTimeUnit}:${minuteTimeUnit}:${secondTimeUnit}`);
-    }, oneSecondIntervalUnitFastDebug);
+        if (hourTimeUnit === maxHourTimeUnit) {
+          hourTimeUnit = 0;
+        }
+
+        // console.log("Second: ", secondTimeUnit);
+
+        // console.log(`${hourTimeUnit}:${minuteTimeUnit}:${secondTimeUnit}`);
+      }, oneSecondIntervalUnitFastDebug);
+    }
   }
   /**
    *
@@ -219,52 +316,78 @@
     let y1 = 0;
     let y2 = 0;
 
-    element.onmousedown =
-      /**
-       *
-       * @param {MouseEvent} event
-       */
-      function (event) {
-        const { pageX, pageY } = event;
-        x1 = pageX;
-        y1 = pageY;
+    if (element) {
+      element.onmousedown =
+        /**
+         *
+         * @param {MouseEvent} event
+         */
+        function (event) {
+          const { pageX, pageY } = event;
+          x1 = pageX;
+          y1 = pageY;
 
-        window.onmousemove =
-          /**
-           *
-           * @param {MouseEvent} event
-           */
-          function (event) {
-            const { pageX, pageY } = event;
-            x2 = pageX;
-            y2 = pageY;
+          window.onmousemove =
+            /**
+             *
+             * @param {MouseEvent} event
+             */
+            function (event) {
+              const { pageX, pageY } = event;
+              x2 = pageX;
+              y2 = pageY;
 
-            const deltaX = x2 - x1;
-            x1 = x2;
+              const deltaX = x2 - x1;
+              x1 = x2;
 
-            const elementLeftNumber = getPixelNumber(element.style.left);
+              const elementLeftNumber = getPixelNumber(element.style.left);
 
-            console.log("deltaX: ", deltaX);
-            console.log("elementLeftNumber: ", elementLeftNumber);
+              console.log("deltaX: ", deltaX);
+              console.log("elementLeftNumber: ", elementLeftNumber);
 
-            const s = `calc(50% + ${elementLeftNumber + deltaX}px)`;
+              const s = `calc(50% + ${elementLeftNumber + deltaX}px)`;
 
-            console.log("s: ", s);
-            element.style.left = `calc(50% + ${
-              elementLeftNumber + deltaX
-            }px)px`;
-          };
+              console.log("s: ", s);
+              element.style.left = `calc(50% + ${
+                elementLeftNumber + deltaX
+              }px)px`;
+            };
 
-        window.onmouseup =
-          /**
-           *
-           * @param {MouseEvent} event
-           */
-          function (event) {
-            window.onmousemove = null;
-            element.onmousedown = null;
-          };
+          window.onmouseup =
+            /**
+             *
+             * @param {MouseEvent} event
+             */
+            function (event) {
+              window.onmousemove = null;
+              element.onmousedown = null;
+            };
+        };
+    }
+  }
+
+  /**
+   *
+   * @param {HTMLElement} circleBarMinute
+   */
+  function circleBarMinuteOnclick(circleBarMinute) {
+    if (circleBarMinute) {
+      circleBarMinute.onclick = function () {
+        toggleElementClassName(circleBarMinute, classes.active);
       };
+    }
+  }
+
+  /**
+   *
+   * @param {HTMLElement} circleBarHour
+   */
+  function circleBarHourOnclick(circleBarHour) {
+    if (circleBarHour) {
+      circleBarHour.onclick = function () {
+        toggleElementClassName(circleBarHour, classes.active);
+      };
+    }
   }
 
   function main() {
@@ -278,8 +401,17 @@
     const barSecond = window.document.getElementsByClassName(
       classes.barSecond
     )[0];
+    const circleBarMinute = window.document.getElementsByClassName(
+      classes.circleBarMinute
+    )[0];
+    const circleBarHour = window.document.getElementsByClassName(
+      classes.circleBarHour
+    )[0];
 
     clockOnclick(clock, barMinute, centerPoint);
+    circleBarMinuteOnclick(circleBarMinute);
+    circleBarHourOnclick(circleBarHour);
+
     barSecondClockWiseRotateActive(barSecond);
 
     dragElement(barMinute);
